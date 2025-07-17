d70abcb72e65f6dd17cfbf0423167679c74d0b34
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2025-07-17 13:01:27 +0700

    fuck kde plasma

--- logout.sh
+++ logout.sh
@@ -3,8 +3,6 @@ if pgrep -x "Hyprland" > /dev/null; then
     killall Hyprland
 elif pgrep -x "sway" > /dev/null; then
     killall sway
-elif pgrep -x "kwin_wayland" > /dev/null; then
-    killall kwin_wayland
 else
     quit
 fi
